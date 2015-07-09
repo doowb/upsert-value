25053b10e1091a4fe1cb7950c79a8a41600c3918
Brian Woodward <brian.woodward@gmail.com>
2015-07-09 16:07:52 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * upsert-value <https://github.com/doowb/upsert-value>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
